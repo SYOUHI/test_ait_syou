5283b3783bb76fe44b9d5a49c39818a7939823a7
Darren.s@gcsystem.co.jp <Xiao&2441>
2024-05-24 17:27:32 +0900

    template削除

--- ait_template/tool/compress_ait_package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# define zipfile name
-FILE_NAME=$(basename "$(realpath "$(dirname "$0")/../")").zip
-
-# make zipfile
-cd "$(dirname "$0")"/../deploy/container/
-zip -r "../../$FILE_NAME" ./*
